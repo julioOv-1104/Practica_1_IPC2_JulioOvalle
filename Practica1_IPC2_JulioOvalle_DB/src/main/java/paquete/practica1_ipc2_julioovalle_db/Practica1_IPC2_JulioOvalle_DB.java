ba6ae12b29ba1f729904eed51668dd0e1476d728
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2025-08-07 12:52:00 -0600

    Commit desde la primer rama: Creacion de proyecto en NetBeans

---
+++ Practica1_IPC2_JulioOvalle_DB/src/main/java/paquete/practica1_ipc2_julioovalle_db/Practica1_IPC2_JulioOvalle_DB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package paquete.practica1_ipc2_julioovalle_db;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica1_IPC2_JulioOvalle_DB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
